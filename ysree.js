27a21f0ba45e98c279ddf1d0d3d41697a31e4ad0
yushuai <shuai.yu@jcy-tech.com>
2024-06-06 17:29:16 +0800

    feat: 封装了day.js

--- ysree.js
+++ ysree.js
@@ -1,2 +1,4 @@
-export { Util1 } from './src/general/general.js';
 export * from './src/general-test/index.js';
+
+export { Util1 } from './src/general/general.js';
+export { Ysday } from './src/general/ysday.js';
